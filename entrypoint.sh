f35f5c4b2c967426879624b2251f55f152cd317b
Tim <65774333+timthedev07@users.noreply.github.com>
2021-09-07 20:59:39 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,8 +23,8 @@ git config --global user.name "$INPUT_USER_NAME"
 git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://x-access-token:$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
 
 echo "Copying contents to git repo"
-mkdir -p $CLONE_DIR/$INPUT_DESTINATION_FOLDER
-cp -R "$INPUT_SOURCE_FILE" "$CLONE_DIR/"
+mkdir -p $CLONE_DIR/
+cp -a "$INPUT_SOURCE_FILE" "$CLONE_DIR/"
 cd "$CLONE_DIR"
 
 if [ ! -z "$INPUT_DESTINATION_BRANCH_CREATE" ]
